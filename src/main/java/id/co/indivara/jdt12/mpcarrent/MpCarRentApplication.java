d56759706a88fba47943b4f10ee360605d0bbef1
rimbadi <rimbadi26@gmail.com>
2023-06-14 10:39:15 +0700

    first commit

---
+++ src/main/java/id/co/indivara/jdt12/mpcarrent/MpCarRentApplication.java
@@ -0,0 +1,13 @@
+package id.co.indivara.jdt12.mpcarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpCarRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpCarRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/indivara/jdt12/mpcarrent/MpCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.indivara.jdt12.mpcarrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpCarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

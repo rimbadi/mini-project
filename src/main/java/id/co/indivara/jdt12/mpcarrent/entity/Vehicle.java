82111e54b131772efba15b45e57f44273d1f6dcf
rimbadi <rimbadi26@gmail.com>
2023-06-18 20:10:05 +0700

    commit entity vehilce

--- src/main/java/id/co/indivara/jdt12/mpcarrent/entity/Vehicle.java
+++ src/main/java/id/co/indivara/jdt12/mpcarrent/entity/Vehicle.java
@@ -1,25 +1,18 @@
 package id.co.indivara.jdt12.mpcarrent.entity;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 public class Vehicle {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String vehicleId;
-    private String nameVehicle;
-    private String priceVehicle;
-
+    private String vehicleName;
+    private String vehicleBrand;
+    private Double vehiclePrice;
 }

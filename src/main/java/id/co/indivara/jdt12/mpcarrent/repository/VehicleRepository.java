5ab57b0c544f8db02a9e82b7fae5cfe67486803c
rimbadi <rimbadi26@gmail.com>
2023-06-20 12:20:05 +0700

    commit InvoiceRepository and VehicleRepository

--- src/main/java/id/co/indivara/jdt12/mpcarrent/repository/VehicleRepository.java
+++ src/main/java/id/co/indivara/jdt12/mpcarrent/repository/VehicleRepository.java
@@ -2,6 +2,8 @@ package id.co.indivara.jdt12.mpcarrent.repository;
 
 import id.co.indivara.jdt12.mpcarrent.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
 }
